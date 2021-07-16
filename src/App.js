e255fff87e119949551a6046484878863c089598
Fede <84208153+Federicoluna1@users.noreply.github.com>
2021-07-16 14:28:31 -0300

    Last version

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Listado from './components/listado';
+
+
+function App() {
+  return (
+    <div className="App">
+          <Listado />
+    </div>
+  );
+}
+
+export default App;
