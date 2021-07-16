8eb8e2c2bbfda2d9ddeb5fb87a88457d54d62876
Fede <84208153+Federicoluna1@users.noreply.github.com>
2021-07-16 14:34:43 -0300

    Add files via upload

---
+++ src/components/listado.jsx
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import React from 'react';
+import './listado.css';
+
+export default function Listado() {
+
+    const [data, setData] = React.useState([]);
+
+    const fetchData = async () => {
+        const respuesta = await axios.get('https://localhost3000/libro');
+        if (respuesta.status === 200) {
+          console.log(respuesta.data);
+          setData(respuesta.data);
+        }
+      };
+    
+      React.useEffect(() => {
+        fetchData();
+      }, []);
+
+
+        const lista = data.map((libro)=>{
+            return (
+                <div className="card" key={libro.id}>
+                    <div className="cardLibro">{libro.titulo}</div>
+                 </div>
+            )
+            })
+
+        return (
+            <div className="container">
+                <h1>Libros disponibles</h1>
+                <ul>
+                    {lista}
+                </ul>
+            </div>
+        )
+    }
