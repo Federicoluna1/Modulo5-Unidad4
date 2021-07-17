a4f1169c0bd8e2a76e3266d478eb53326a9584ab
Fede <84208153+Federicoluna1@users.noreply.github.com>
2021-07-17 15:52:25 -0300

    Book List app

--- src/components/listado.jsx
+++ src/components/listado.jsx
@@ -7,7 +7,7 @@ export default function Listado() {
     const [data, setData] = React.useState([]);
 
     const fetchData = async () => {
-        const respuesta = await axios.get('https://localhost3000/libro');
+        const respuesta = await axios.get('https://jsonplaceholder.typicode.com/comments');
         if (respuesta.status === 200) {
           console.log(respuesta.data);
           setData(respuesta.data);
@@ -22,15 +22,15 @@ export default function Listado() {
         const lista = data.map((libro)=>{
             return (
                 <div className="card" key={libro.id}>
-                    <div className="cardLibro">{libro.titulo}</div>
+                    <div className="cardLibro">{libro.name}</div>
                  </div>
             )
             })
 
         return (
-            <div>
-                <h1>Libros disponibles</h1>
-                {lista}
+            <div className = 'container'>
+              <h1 >Libros disponibles</h1>
+              {lista}
             </div>
         )
     }
